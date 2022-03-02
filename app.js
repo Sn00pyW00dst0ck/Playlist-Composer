af02aeddddf4663d46a1fc1719abff07cf7d29d7
Sn00pyW00dst0ck <gabealdous@gmail.com>
2022-03-01 21:19:18 -0500

    Second Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { append } = require('express/lib/response');
+const path = require('path');
+
+const PORT = 3000;
+const APP = express();
+
+APP.use(express.static(path.join(__dirname, 'Public')));
+APP.use(express.json({ limit: '1mb', type: '*/*'}));
+
+APP.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+});
+
+
+APP.listen(PORT, () =>  {console.log("Server is listening on port " + PORT)});
