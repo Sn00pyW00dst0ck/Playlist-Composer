92ff6c9fdcef471dfb0ac7fda87a6601e6ca8958
Sn00pyW00dst0ck <gabealdous@gmail.com>
2022-03-27 16:56:58 -0400

    Update for Searchbars

--- client/src/components/Searchbar.js
+++ client/src/components/Searchbar.js
@@ -2,14 +2,12 @@ import useFetch from '../customHooks/useFetch';
 import React, {useState} from 'react'
 import styles from "./Searchbar.module.css"
 
-function Searchbar ({users, setUsers}) {
-    
-
+function Searchbar ({setUser}) {
     return (
         <>
         <div className="search-bar">
             {/** Using JS Spread Operator to append newest item to the array */}
-            <input type="text" onChange={e => setUsers([...users, e.target.value])} placeholder="Enter a username"/>
+            <input type="text" onChange={e => setUser(e.target.value)} placeholder="Enter a username"/>
         </div>
         </>
     );
--- client/src/pages/Select-Users-Page.js
+++ client/src/pages/Select-Users-Page.js
@@ -1,43 +1,60 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {useState} from 'react'
 import useFetch from '../customHooks/useFetch';
 import Searchbar from '../components/Searchbar'
 
 // FILLER ADD SEARCH BAR AND CREATE SEPARATE CSS FILE FOR IT
 function SelectUsersPage() {
-    const [users, setUsers] = useState([]);
+
+    //A use state for each user
+    const [user1, setUser1] = useState(null);
+    const [user2, setUser2] = useState(null);
+    const [user3, setUser3] = useState(null);
+    const [user4, setUser4] = useState(null);
+
+    //The options for the fetch request and the actual fetch hook
     const [options, setOptions] = useState(null);
     const {isLoading, responseData, fetchError} = useFetch("/api/create-playlist", options);
+    
+    /*
+    Optional console log statements for debugging in browser
+    console.log(isLoading);
+    console.log(responseData);
+    console.log(fetchError);
+    */
 
     return(
         <>
-        /*
-        In Searchbar.js, when text is entered we call setUsers function to update the user list
-        Create a button that when clicked uses the 'useFetch' hook and calls our backend API
-        "/api/create-playlist"
-        
-        */
         {/* Main Content */}
         <section className="landing-main">
             <h1>Select Other Users To Customize Playlist For</h1>
             
-                <Searchbar users={users} setUsers={setUsers} />
-                <Searchbar users={users} setUsers={setUsers} />
-                <Searchbar users={users} setUsers={setUsers} />
-                <Searchbar users={users} setUsers={setUsers} />
+            {/* Put 4 Searchbars on the page, with key's 0, 1, 2, and 3, so they each can update one specific name */}
+            <Searchbar setUser={setUser1} />
+            <Searchbar setUser={setUser2} />
+            <Searchbar setUser={setUser3} />
+            <Searchbar setUser={setUser4} />
 
-                <button onClick={() => setOptions({
+            
+            {/* Button that changes the options for the fetch and triggers the useFetch hook to call our backend */}
+            <button onClick={() => {
+                //Filter the users to remove null or undefined values
+                let users = [user1, user2, user3, user4].filter((e) =>  {
+                    return e != null;
+                });
+                
+                //Set the fetch options (which triggers the useFetch hook to fire)
+                setOptions({
                     method: "POST",
                     headers:  {
                         'Content-Type': 'application/json'
                     },
-                    body: JSON.stringify({user:users})
-        
-                })}>Submit</button>
-        
+                    body: JSON.stringify({users:users})
+                });
                 
-        </section>
+            }}>Submit</button>
         
+        </section>
         </>
     ) 
 }
